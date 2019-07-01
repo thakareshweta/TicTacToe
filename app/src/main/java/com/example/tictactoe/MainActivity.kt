dd4c088b076dec31235e72094075c275fa28b65d
Shweta <shweta.thakare@vividseats.com>
2019-07-01 15:24:14 -0500

    Add more winning conditions

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -108,6 +108,13 @@ class MainActivity : AppCompatActivity() {
         if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
             winner = 1;
         }
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1;
+        }
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1;
+        }
+
         if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
             winner = 2;
         }
@@ -124,6 +131,12 @@ class MainActivity : AppCompatActivity() {
             winner = 2;
         }
         if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winner = 2;
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2;
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
             winner = 2;
         }
 
